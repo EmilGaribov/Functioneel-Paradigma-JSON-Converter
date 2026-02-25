476f9ee1dbf805b8814662a421f8d7880fcd5502
EmilGaribov <e.garibov@student.han.nl>
2026-02-25 12:07:03 +0100

    Custom JSON

--- scr/main.hs
+++ scr/main.hs
@@ -1,57 +1,12 @@
 import Distribution.Utils.Json (Json)
+import Distribution.SPDX (LicenseId(JSON))
 
 {- TIJDELIJKE TEXT LATER WEGHALEN
-Haskell
-Json converter naar iets (start simple) als tijd over doe meer anders max 6 volgens micheal
-{platte txt}
-{pretty maken}
-etc
-
-Het doel van de opdracht is om:
-
-De werking van functionele programmeertalen te begrijpen en functionele concepten in praktijk te brengen.
-(Dus als het niet werk kan je nog voldoen scoren als je laat zien dat je goed bezig was vandaar git)
-Een oplossing te programmeren die gebaseerd is op het functionele paradigma.
-(Dat je Functionle taal gebruikt?)
-Functionele concepten bewust te koppelen aan specifieke onderdelen van je uitgeprogrammeerde algoritme.
-(Lesstof toepassen bij mijn algoritmes?)
-Je bevindingen helder te verwoorden in een rapport dat voldoet aan de AIM-controlekaart.
-(Het opschrijft (Documenteerd) volgens HAN standaarden (Template))
-
-------Dit moeten we onderzoeken.------waarschijnlijk ook iets over schrijven.
-Zuiverheid (pure functions)
-First-class functions
-Higher-order functions
-Immutability
-Recursie
-Lazy evaluation
-Pattern matching
-
 leg uit welke functionele concepten je hebt toegepast en hoe ze bijdragen aan de oplossing.
 Schrijf een kort rapport (ongeveer 4-6 pagina’s)
 -}
 
 {-
-You can omit the header of a method
-function a = 3 + a works alone since it READS this as
-
-function:: Int -> Int
-function a =  3 + a
-
-You cannot normally put print inside a normal function like doEuclidische
-Because print is an IO action, and your function is pure
-
-ONLY THESE are pure
-+
--
-\*
-/
-mod
-if statements
-recursion
-pattern matching
-list operations
-
 Schrijf een JSON-parser die JSON-bestanden inleest en omzet naar een datastructuur.
 Maak gebruik van recursie en pattern matching om geneste objecten en arrays correct te verwerken.
 
@@ -63,16 +18,6 @@ Omdat JSON genest kan zijn
     "name": "Emil" <-- NESTED
   }
 }
----------
-IS DIT pattern matching?
-
-Als tekst begint met '{' → Object
-Als tekst begint met '[' → Array
-Als tekst begint met '"' → String
-Als tekst = true/false → Bool
-Als tekst = null → Null
-Anders →  value/int ?
-
 -}
 -- JSON input → Parse → Transform → Plain text → Print / Save
 -- What type is JSON? --> convert --> String
@@ -81,18 +26,32 @@ main :: IO ()
 main = do
   jsonFileContent <- readFile "inputfile.json" -- of dit mag ligt aan hoeveel Functinal code ik nog kan toepassen
   print jsonFileContent -- "{\n  \"name\": \"Emil\",\n  \"age\": 24\n}\n"
-  print (jsonPaser jsonFileContent) -- "name:Emil,age:24"
+  print (jsonToPlainText jsonFileContent) -- "name:Emil,age:24"
 
-jsonPaser :: String -> String 
-jsonPaser [] = []
-jsonPaser (x : xs)
-  | x == '\n' = jsonPaser xs 
-  | x == ' ' = jsonPaser xs 
-  | x == '{' = jsonPaser xs
-  | x == '}' = jsonPaser xs
-  | x == '"' = jsonPaser xs 
-  | x == '\\' = jsonPaser xs
-  | otherwise = x : jsonPaser xs
+jsonToPlainText :: String -> String 
+jsonToPlainText [] = []
+jsonToPlainText (x : xs)
+  | x == '\n' = jsonToPlainText xs 
+  | x == ' ' = jsonToPlainText xs 
+  | x == '{' = jsonToPlainText xs
+  | x == '}' = jsonToPlainText xs
+  | x == '"' = jsonToPlainText xs 
+  | x == '\\' = jsonToPlainText xs
+  | otherwise = x : jsonToPlainText xs
 
-  -- Non-exhaustive patterns in function jsonPaser????
-  -- Ik kreeg deze omdat ik de otherwise niet had :P
+-- Schrijf een JSON-parser die JSON-bestanden inleest en omzet naar een datastructuur.
+
+--datastuctuur (Patternmatching)
+-- dit is een Haskell algebraic data type (ADT).== Custom type
+data JSON
+  = JObject [(String, JSON)] 
+  | JArray [JSON]             
+  | JString String            
+  | JNumber Double            
+  | JBool Bool                
+  | JNull                     
+  deriving (Show, Eq)
+
+
+
+-- plainTextToJson :: String -> String
