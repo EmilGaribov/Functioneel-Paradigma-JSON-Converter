d79c54a9da8494208e4ebffb09015a20b232a3ef
EmilGaribov <e.garibov@student.han.nl>
2026-03-12 16:58:25 +0100

    Spelling by testfile

--- src/test/Test.hs
+++ src/test/Test.hs
@@ -17,31 +17,31 @@ import JSONTypes ( JSON(JString,JNumber, JArray, JObject) )
 import JSONParser (parseValue, parseString , parseArray , parseObject , splitTopLevel)
 
 
-testJNumber :: Test
-testJNumber = TestCase (assertEqual "Check of getal klopt" --wat je checkt
+testJNumber :: Test -- dit is meer een showcase test 
+testJNumber = TestCase $ assertEqual "Check of getal klopt" --wat je checkt
                         (JNumber 42.0)   --Verwacht
-                        (parseValue "42")) --Echte antwoord
+                        (parseValue "42") --Echte antwoord
 
 testStringEmpty :: Test
-testStringEmpty = TestCase (assertEqual "Check of hij leeg is" "" (parseString ""))
+testStringEmpty = TestCase $ assertEqual "Check of hij leeg is" "" (parseString "")
 
 testStringEscapedCharacters :: Test
 testStringEscapedCharacters =
-    TestCase (assertEqual "Check if escaped charc stays escaped after becoming JString"
+    TestCase $ assertEqual "Check if escaped charc stays escaped after becoming JString"
          "Hallo\nWereld"
-         (parseString "\"Hallo\\nWereld\""))
+         (parseString "\"Hallo\\nWereld\"")
 
 testArrayEmpty :: Test
 testArrayEmpty = TestCase $ assertEqual "Check of de array leeg is" (JArray []) (parseArray "[]" )
 
 testArrayNested :: Test
-testArrayNested = TestCase $ assertEqual "nested array parsing" (JArray [JArray [JNumber 0.0]]) (parseValue "[[0.0]]")
+testArrayNested = TestCase $ assertEqual "Nested array parsing" (JArray [JArray [JNumber 0.0]]) (parseValue "[[0.0]]")
 
 testObjectEmpty :: Test
 testObjectEmpty = TestCase $ assertEqual "Controleer of hij leeg is" (JObject []) (parseObject "{}")
 
 testObjectNested :: Test
-testObjectNested = TestCase $ assertEqual "Object word correct geparsed" 
+testObjectNested = TestCase $ assertEqual "Object wordt correct geparsed" 
     (JObject [("outer", JObject [("inner", JString "kip")])])
     (parseValue "{\"outer\": {\"inner\": \"kip\"}}")
 
@@ -63,16 +63,16 @@ testNestedNumbersArraysAndObjects = TestCase $ assertEqual "Mix van numbers, obj
 
 tests :: Test
 tests = TestList [
-                TestLabel "Number Test" testJNumber,
-                TestLabel "Empty String Test" testStringEmpty,
-                TestLabel "Ecscaped String Test" testStringEscapedCharacters,
-                TestLabel "Is de Array leeg?" testArrayEmpty,
+                TestLabel "Number test" testJNumber,
+                TestLabel "Empty string test" testStringEmpty,
+                TestLabel "Escaped string test" testStringEscapedCharacters,
+                TestLabel "Is de array leeg?" testArrayEmpty,
                 TestLabel "Nested array word geparsed" testArrayNested,
-                TestLabel "Kijk of Object leeg is" testObjectEmpty,
-                TestLabel "Nested Object Parsen" testObjectNested,
+                TestLabel "Kijk of object leeg is" testObjectEmpty,
+                TestLabel "Nested object parsen" testObjectNested,
                 TestLabel "Test of de functie array commas goed verwerkt" testNestedArraySplit,
                 TestLabel "Test of de commas in het object correct worden verwerkt " testNestedObjectSplit,
-                TestLabel "" testNestedNumbersArraysAndObjects
+                TestLabel "Test die een mix van numbers, objects en arrays verwerkt" testNestedNumbersArraysAndObjects
                  ]
 
 main :: IO ()
